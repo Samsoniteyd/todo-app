354d5dfdadb907d40e824876a02dfb1034eac7f7
AMOO <amoosamson@gmail.com>
2024-09-11 23:08:19 +0100

    to deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,18 @@
 module.exports = {
+    parser: '@typescript-eslint/parser', // Use TypeScript parser
+    parserOptions: {
+      ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+      sourceType: 'module', // Allows for the use of imports
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'next/core-web-vitals',
+    ],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      // Custom ESLint rules
     },
   };
